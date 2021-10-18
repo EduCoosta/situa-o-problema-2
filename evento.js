b1e2d7939872aa15466dbbfd910fb2eed35bc029
EduCoosta <91344813+EduCoosta@users.noreply.github.com>
2021-10-18 12:02:18 -0400

    Add files via upload

---
+++ evento.js
@@ -0,0 +1,29 @@
+let dataevento = 13/10/21;
+
+if(dataevento > 13/10/21) {
+    console.log("Ops, esse evento ja foi realizado!")
+} else {
+    console.log("Dia do cadastro, confirmado")
+}
+
+let idade = 18;
+
+if(idade >= 18) {
+    console.log("Idade confirmada, acima de 18 anos")
+} else {
+    console.log("Evento para pessoas acima de 18 anos!!!")
+}
+
+let quantidadep = 100;
+
+if(quantidadep <= 100) {
+    console.log("Capacidade de pessoas, não completa. Cadastro realizado!")
+} else {
+console.log("Excedido a capacidade maxima de pessoas")
+}
+
+let nomepp = "Eduardo";
+
+if(nomepp.length) {
+    console.log("Nome cadastrado")
+}
